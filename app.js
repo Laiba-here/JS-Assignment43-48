39211d3a21f9111f0170096d5486d763b3a2b48e
Laiba-here <nazlaiba230@gmail.com>
2024-10-11 11:05:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//          .... QUESTION 1.... 
+// function showAlert() {
+//     alert("You clicked the link!");
+// }
+
+//          .... QUESTION 2....
+// function showMessage() {
+//     alert("Thanks for purchasing a phone from us.");
+// }
+
+//          .... QUESTION 3....
+// function deleteRow(button) {
+//     var row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+//     updateIndexes();
+// }
+
+// function updateIndexes() {
+//     var table = document.getElementById("studentTable");
+//     var rows = table.getElementsByTagName("tr");
+//     for (var i = 1; i < rows.length; i++) {
+//         rows[i].getElementsByTagName("td")[0].innerHTML = i - 1; 
+//     }
+// }
+
+
+//          .... QUESTION 4....
+// function changeImage() {
+//     document.getElementById('myImage').src = 'image2.jpg'; 
+// }
+// function resetImage() {
+//     document.getElementById('myImage').src = 'image1.jpg'; 
+// }
+
+//          .... QUESTION 5....
+// let counterValue = 0;
+//         function increaseCounter() {
+//             counterValue++;
+//             document.getElementById('counter').innerText = counterValue;
+//         }
+//         function decreaseCounter() {
+//             counterValue--;
+//             document.getElementById('counter').innerText = counterValue;
+//         }
